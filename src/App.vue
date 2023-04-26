a8d4b0f6191c905aa645972ed9eadf152eac89ea
문준호 <tjsduq0423@naver.com>
2023-04-26 16:57:06 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<TestVue></TestVue>
+</template>
+
+<script setup>
+import TestVue from './components/TestVue.vue';
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TestVue.vue
@@ -0,0 +1,14 @@
+<template>
+	<button @click="incresement">counter : {{ counter }}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+function incresement() {
+	counter.value++;
+}
+</script>
+
+<style lang="scss" scoped></style>

aa599577070307280ae5d3e92db3bc32656df7d6
문준호 <tjsduq0423@naver.com>
2023-04-27 09:43:59 +0900

    edit directory structure

---
+++ FrontEnd/src/components/TestVue.vue
@@ -0,0 +1,17 @@
+<template>
+	<button @click="printEventInfo('hello', $event)">inline event handler</button>
+	<hr />
+	<input type="text" @keyup="onKeyupHandler" />
+</template>
+
+<script setup>
+function printEventInfo(msg, event) {
+	console.log('message : ', msg);
+	console.log('event.target', event.target);
+}
+function onKeyupHandler(event) {
+	console.log('event.key', event.key);
+}
+</script>
+
+<style lang="scss" scoped></style>

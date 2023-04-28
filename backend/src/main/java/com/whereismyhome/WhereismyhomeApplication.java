aedf3acc2654667b4a53771d3d0ae01ee1b35148
Han5ung <66876894+HanSungHyeon@users.noreply.github.com>
2023-04-28 10:03:55 +0900

    init
    초기

---
+++ backend/src/main/java/com/whereismyhome/WhereismyhomeApplication.java
@@ -0,0 +1,13 @@
+package com.whereismyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereismyhomeApplication.class, args);
+	}
+
+}

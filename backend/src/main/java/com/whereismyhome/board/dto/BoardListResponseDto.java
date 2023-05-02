e230d1e095d8ba2ed194148c3a54243cb6f7d20e
한성현 <hsh5349@naver.com>
2023-05-02 16:12:34 +0900

    add : 게시글 목록 조회 반환 dto 생성

---
+++ backend/src/main/java/com/whereismyhome/board/dto/BoardListResponseDto.java
@@ -0,0 +1,18 @@
+package com.whereismyhome.board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardListResponseDto {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createtime;
+    private String responseContent;
+
+}

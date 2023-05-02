9201ae56f9b58acc9233ef4da3d0ef708624f53f
한성현 <hsh5349@naver.com>
2023-05-02 09:11:03 +0900

    add : Member 엔티티 생성

---
+++ backend/src/main/java/com/whereismyhome/member/entity/Member.java
@@ -0,0 +1,28 @@
+package com.whereismyhome.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member{
+    @Id
+    private String id;
+
+    @Column(nullable = true, length = 45)
+    private String password;
+
+    @Column(nullable = true, length = 45)
+    private String name;
+
+    @Column(nullable = true, length = 45)
+    private String email;
+}

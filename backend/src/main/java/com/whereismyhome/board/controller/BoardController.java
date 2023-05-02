94dab3bf2e71327b23ed649cb21e628eae6ae7d4
한성현 <hsh5349@naver.com>
2023-05-02 16:12:04 +0900

    add : 게시글 삭제 및 본인이 쓴 게시글 목록 조회

--- backend/src/main/java/com/whereismyhome/board/controller/BoardController.java
+++ backend/src/main/java/com/whereismyhome/board/controller/BoardController.java
@@ -1,18 +1,18 @@
 package com.whereismyhome.board.controller;
 
+import com.whereismyhome.board.dto.BoardListResponseDto;
 import com.whereismyhome.board.dto.BoardPostDto;
 import com.whereismyhome.board.entity.Board;
 import com.whereismyhome.board.mapper.BoardMapper;
 import com.whereismyhome.board.service.BoardService;
-import com.whereismyhome.jwt.JwtProvider;
 import com.whereismyhome.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,15 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class BoardController {
     private final BoardService boardService;
-    private final JwtProvider jwtProvider;
     private final BoardMapper mapper;
+
+    //게시글 등록
     @PostMapping("/regist")
     public ResponseEntity regist(@RequestBody BoardPostDto postDto) {
         Board board = mapper.boardPostDtoToBoard(postDto);
         Member member = new Member();
-        member.setId(postDto.getId());
+        member.setId(postDto.getMemberId());
         board.setMember(member);
         boardService.regist(board);
         return ResponseEntity.ok().build();
+    }
+
+    //게시글 삭제
+    //게시글 번호
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") int id) {
+        boardService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    //게시글 목록 조회
+    //자신이 쓴 게시글만 조회
+    @GetMapping("/{id}")
+    public ResponseEntity boardList(@PathVariable("id") String id) {
+        List<Board> boards = boardService.boardList(id);
+        List<BoardListResponseDto> boardListResponseDtos = mapper.boardListToBoardListResponseDto(boards);
+
+        return new ResponseEntity(boardListResponseDtos, HttpStatus.OK);
     }
 }

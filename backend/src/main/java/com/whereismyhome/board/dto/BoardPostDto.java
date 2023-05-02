d8ce47a471c778ce940cfdee4308b073b319e64f
한성현 <hsh5349@naver.com>
2023-05-02 12:14:27 +0900

    add: 게시판등록dto

---
+++ backend/src/main/java/com/whereismyhome/board/dto/BoardPostDto.java
@@ -0,0 +1,13 @@
+package com.whereismyhome.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardPostDto {
+    private String title;
+    private String content;
+}

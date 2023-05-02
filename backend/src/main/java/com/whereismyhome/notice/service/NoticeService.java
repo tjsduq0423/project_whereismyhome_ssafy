42125b96dc47a72ca0de67bb63d4f77e419d4754
한성현 <hsh5349@naver.com>
2023-05-02 17:28:42 +0900

    add : 공지사항 service 생성

---
+++ backend/src/main/java/com/whereismyhome/notice/service/NoticeService.java
@@ -0,0 +1,17 @@
+package com.whereismyhome.notice.service;
+
+import com.whereismyhome.notice.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+    
+    //공지사항 작성
+    
+
+    //공지사항 전체 조회
+}

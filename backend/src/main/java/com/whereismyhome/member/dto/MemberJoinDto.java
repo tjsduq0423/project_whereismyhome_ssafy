72879328913cc1aad148c19d245cfe177664a70f
한성현 <hsh5349@naver.com>
2023-05-02 09:22:13 +0900

    update : 회원가입에 요청받을 dto 필드 추가

--- backend/src/main/java/com/whereismyhome/member/dto/MemberJoinDto.java
+++ backend/src/main/java/com/whereismyhome/member/dto/MemberJoinDto.java
@@ -1,8 +1,10 @@
 package com.whereismyhome.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MemberJoinDto {
     private String id;
     private String password;

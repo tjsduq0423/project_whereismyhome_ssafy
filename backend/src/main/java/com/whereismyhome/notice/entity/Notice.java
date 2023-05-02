ba73687b2e7e28f1b6296259dcaf04c228c08520
한성현 <hsh5349@naver.com>
2023-05-02 17:24:32 +0900

    add : 공지사항 엔티티 생성

---
+++ backend/src/main/java/com/whereismyhome/notice/entity/Notice.java
@@ -0,0 +1,32 @@
+package com.whereismyhome.notice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @CreatedDate
+    @Column
+    private LocalDateTime createtime = LocalDateTime.now();
+}

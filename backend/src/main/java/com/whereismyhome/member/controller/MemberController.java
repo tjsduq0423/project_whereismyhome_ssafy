22752868453a0da40954264a322b730dffd35e79
한성현 <hsh5349@naver.com>
2023-05-02 10:23:19 +0900

    add : Controller 회원가입 생성

---
+++ backend/src/main/java/com/whereismyhome/member/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.whereismyhome.member.controller;
+
+
+import com.whereismyhome.member.dto.MemberJoinDto;
+import com.whereismyhome.member.mapper.MemberMapper;
+import com.whereismyhome.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+    //회원 가입
+    @PostMapping("/join")
+    public ResponseEntity join(@RequestBody MemberJoinDto memberJoinDto) throws IllegalAccessException {
+        memberService.join(mapper.memberJoinDtoToMember(memberJoinDto));
+
+        return ResponseEntity.ok().build();
+    }
+
+
+    //로그인
+
+    //로그아웃
+}

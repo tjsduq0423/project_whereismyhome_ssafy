e38cfb513fa07811302148d41130004e5cee4900
한성현 <hsh5349@naver.com>
2023-05-02 17:29:30 +0900

    add : 공지사항 등록 서비스 생성

--- backend/src/main/java/com/whereismyhome/notice/service/NoticeService.java
+++ backend/src/main/java/com/whereismyhome/notice/service/NoticeService.java
@@ -1,5 +1,6 @@
 package com.whereismyhome.notice.service;
 
+import com.whereismyhome.notice.entity.Notice;
 import com.whereismyhome.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NoticeService {
     private final NoticeRepository noticeRepository;
-    
+
     //공지사항 작성
-    
+    public void postNotice(Notice notice) {
+        noticeRepository.save(notice);
+    }
 
     //공지사항 전체 조회
 }

ea2db0395406a65acdab1668777e2b2a15601dc3
한성현 <hsh5349@naver.com>
2023-05-02 10:21:49 +0900

    add : MemberService 회원 id 중복검사 및 회원가입 메서드 생성

--- backend/src/main/java/com/whereismyhome/member/service/MemberService.java
+++ backend/src/main/java/com/whereismyhome/member/service/MemberService.java
@@ -1,12 +1,35 @@
 package com.whereismyhome.member.service;
 
+import com.whereismyhome.member.entity.Member;
 import com.whereismyhome.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    //회원가입
+    public void join(Member member) throws IllegalAccessException {
+        verifiedMember(member.getId());
+
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        memberRepository.save(member);
+    }
+
+    //회원 id 중복 검사
+    //회원찾기
+    public void verifiedMember(String id) throws IllegalAccessException {
+        Optional<Member> memberId = memberRepository.findById(id);
+
+        if(memberId.isPresent()) {
+            throw new IllegalAccessException("중복된 아이디입니다.");
+        }
+    }
 }

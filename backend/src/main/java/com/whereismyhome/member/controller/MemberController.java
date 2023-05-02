3e1392925067d389ab511848ea21964527869333
한성현 <hsh5349@naver.com>
2023-05-02 10:53:32 +0900

    add : 회원 컨트롤러 로그인 로그아웃 메서드 생성

--- backend/src/main/java/com/whereismyhome/member/controller/MemberController.java
+++ backend/src/main/java/com/whereismyhome/member/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.whereismyhome.member.controller;
 
 
+import com.whereismyhome.jwt.JwtProvider;
 import com.whereismyhome.member.dto.MemberJoinDto;
+import com.whereismyhome.member.dto.MemberLoginDto;
+import com.whereismyhome.member.entity.Member;
 import com.whereismyhome.member.mapper.MemberMapper;
 import com.whereismyhome.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +17,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtProvider jwtProvider;
 
     //회원 가입
     @PostMapping("/join")
@@ -25,8 +35,36 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody MemberLoginDto loginDto, HttpServletResponse response) {
+        Member member = memberService.findUser(loginDto);
+        boolean check = memberService.checkPassWord(member, loginDto);
 
-    //로그인
+        //비밀번호 체크
+        if (!check) {
+            throw new IllegalStateException("아이디 혹은 비밀번호가 잘 못 되었습니다.");
+        }
+
+        String accessToken = jwtProvider.createAccessToken(member.getId(), member.getRoles());
+//        String encode = URLEncoder.encode("Bearer " + accessToken, StandardCharsets.UTF_8);
+        String encode = URLEncoder.encode(accessToken, StandardCharsets.UTF_8);
+
+        Cookie cookie = new Cookie("Authorization", encode);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().body("로그인 성공");
+    }
 
     //로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("Authorization", null);
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+
+        return ResponseEntity.noContent().build();
+    }
 }

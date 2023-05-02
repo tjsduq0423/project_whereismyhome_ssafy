4ac38968ec993bab2781ea60bb879cc58c3ee7c7
한성현 <hsh5349@naver.com>
2023-05-02 09:18:03 +0900

    add : MemberService 추가

---
+++ backend/src/main/java/com/whereismyhome/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.whereismyhome.member.service;
+
+import com.whereismyhome.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}

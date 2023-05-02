fdf5f2633cb3cc203dbe4350257841d7a90b3af4
한성현 <hsh5349@naver.com>
2023-05-02 14:39:17 +0900

    update: 게시글등록dto 수정

--- backend/src/main/java/com/whereismyhome/board/dto/BoardPostDto.java
+++ backend/src/main/java/com/whereismyhome/board/dto/BoardPostDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardPostDto {
+    private String id;
     private String title;
     private String content;
 }

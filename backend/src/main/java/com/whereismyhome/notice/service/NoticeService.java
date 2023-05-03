3547bf1abaefc0b70d12811b45d7f5379e86e522
한성현 <hsh5349@naver.com>
2023-05-03 13:31:06 +0900

    add: 공지사항 상세 조회 및 유효성 검사 로직 생성

--- backend/src/main/java/com/whereismyhome/notice/service/NoticeService.java
+++ backend/src/main/java/com/whereismyhome/notice/service/NoticeService.java
@@ -3,6 +3,7 @@ package com.whereismyhome.notice.service;
 import com.whereismyhome.notice.entity.Notice;
 import com.whereismyhome.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,8 +19,20 @@ public class NoticeService {
     }
 
     //공지사항 전체 조회
-    public List<Notice> noticeList(){
+    public List<Notice> noticeList() {
         List<Notice> list = noticeRepository.findAll();
         return list;
     }
+
+    //공지사항 찾기 및 유효성 검사
+    public Notice findNotice(int id) {
+        return noticeRepository.findById(id)
+                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+    }
+
+    //공지사항 상세 조회
+    public Notice detailNotice(int id) {
+        return findNotice(id);
+    }
+
 }

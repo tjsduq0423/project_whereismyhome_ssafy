ad60b4eb9067ae1e742edbf1007c7588d6a839e1
HanSungHyeon <hsh5349@naver.com>
2023-05-05 03:49:03 +0900

    add: 북마크 dto생성

---
+++ backend/src/main/java/com/whereismyhome/bookmark/dto/BookMarkDto.java
@@ -0,0 +1,12 @@
+package com.whereismyhome.bookmark.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookMarkDto {
+    private Long aptCode;
+}

eccd934b366766a163964fa0f2d0ac8e56cf2de7
HanSungHyeon <hsh5349@naver.com>
2023-05-05 04:46:34 +0900

    #9 add : db에서 북마크 조회할 쿼리 메서드 생성

--- backend/src/main/java/com/whereismyhome/bookmark/repository/BookMarkRepository.java
+++ backend/src/main/java/com/whereismyhome/bookmark/repository/BookMarkRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface BookMarkRepository extends JpaRepository<BookMark,Integer> {
     @Transactional
     @Modifying
     @Query(value = "delete from bookmark where member_id = ? and houseinfo_aptcode = ?", nativeQuery = true)
     void deleteBookMark(String id, long aptCode);
+
+    List<BookMark> findByMember_Id(String id);
 }

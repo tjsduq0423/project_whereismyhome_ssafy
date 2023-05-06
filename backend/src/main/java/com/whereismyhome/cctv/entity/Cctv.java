d5b6482329763db96c549b77f716da505ce8628e
HanSungHyeon <hsh5349@naver.com>
2023-05-06 18:45:12 +0900

    #13 add : cctv 엔티티 생성

---
+++ backend/src/main/java/com/whereismyhome/cctv/entity/Cctv.java
@@ -0,0 +1,27 @@
+package com.whereismyhome.cctv.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "cctv")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Cctv {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String usage;
+
+    @Column
+    private String lat;
+
+    @Column
+    private String lng;
+}

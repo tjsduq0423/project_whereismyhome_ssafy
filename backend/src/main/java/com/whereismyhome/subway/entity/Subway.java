e023966baac39d835620a0339165df05932fb856
HanSungHyeon <hsh5349@naver.com>
2023-05-06 20:45:07 +0900

    #13 add : 학교, 지하철 엔티티 생성

---
+++ backend/src/main/java/com/whereismyhome/subway/entity/Subway.java
@@ -0,0 +1,30 @@
+package com.whereismyhome.subway.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "subway")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subway {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lat;
+
+    @Column
+    private String lng;
+
+    @Column
+    private String phoneNumber;
+}

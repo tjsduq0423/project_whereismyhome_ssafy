a2ecd4a8eebdc3cb1501c96b0370ab4e38782261
HanSungHyeon <hsh5349@naver.com>
2023-05-06 20:27:30 +0900

    #13 add : 거래정보 엔티티 생성

---
+++ backend/src/main/java/com/whereismyhome/hospital/Housedeal.java
@@ -0,0 +1,44 @@
+package com.whereismyhome.hospital;
+
+import com.whereismyhome.houseinfo.entity.HouseInfo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "housedeal")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Housedeal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
+
+    @Column(name = "dealAmount")
+    private String dealAmount;
+
+    @Column(name = "dealYear")
+    private int dealYear;
+
+    @Column(name = "dealMonth")
+    private int dealMonth;
+
+    @Column(name = "dealDay")
+    private int dealDay;
+
+    @Column(name = "area")
+    private String area;
+
+    @Column(name = "floor")
+    private String floor;
+
+    @Column(name = "cancelDealType")
+    private String cancelDealType;
+
+    @ManyToOne
+    @JoinColumn(name = "apt_code")
+    private HouseInfo houseInfo;
+}

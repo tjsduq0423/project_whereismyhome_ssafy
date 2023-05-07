25a487aad4ce3a0410513810455ff2da6050e787
HanSungHyeon <hsh5349@naver.com>
2023-05-07 15:37:59 +0900

    #18 fix: 회원가입 로직 변경 memberRole 엔티티 생성에 따라 회원가입 로직 변경

--- backend/src/main/java/com/whereismyhome/member/service/MemberService.java
+++ backend/src/main/java/com/whereismyhome/member/service/MemberService.java
@@ -4,11 +4,12 @@ import com.whereismyhome.member.dto.MemberJoinDto;
 import com.whereismyhome.member.dto.MemberLoginDto;
 import com.whereismyhome.member.entity.Member;
 import com.whereismyhome.member.repository.MemberRepository;
+import com.whereismyhome.role.entity.MemberRole;
+import com.whereismyhome.role.repository.MemberRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MemberRoleRepository memberRoleRepository;
 
     //회원가입
     public void join(MemberJoinDto postDto) throws IllegalAccessException {
@@ -27,10 +29,16 @@ public class MemberService {
                 .password(passwordEncoder.encode(postDto.getPassword()))
                 .email(postDto.getEmail())
                 .name(postDto.getName())
-                .roles(Collections.singletonList("ROLE_USER"))
+//                .roles(roles)
                 .build();
 
+        MemberRole role = new MemberRole();
+        role.setRole("ROLE_USER");
+        role.setMember(member);
+
+//        member.getRoles().add(role);
         memberRepository.save(member);
+        memberRoleRepository.save(role);
     }
 
     //회원 id 중복 검사

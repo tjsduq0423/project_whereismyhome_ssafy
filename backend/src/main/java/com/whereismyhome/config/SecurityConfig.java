833a461a60048c8a39c4b793cd337416ec53f39f
HanSungHyeon <hsh5349@naver.com>
2023-05-07 17:09:56 +0900

    #18 add: 권한가져오기 유저 권한을 가져와 스프링 시큐리티에 등록하기

--- backend/src/main/java/com/whereismyhome/config/SecurityConfig.java
+++ backend/src/main/java/com/whereismyhome/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,7 +31,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/notice/write", "/board/answer").hasRole("ADMIN")
+                .requestMatchers("/notice/write","board/answer").hasRole("ADMIN")
                 .requestMatchers("/h2/**").permitAll()
                 .anyRequest().permitAll()
                 .and()
--- backend/src/main/java/com/whereismyhome/member/entity/Member.java
+++ backend/src/main/java/com/whereismyhome/member/entity/Member.java
@@ -6,6 +6,7 @@ import com.whereismyhome.role.entity.MemberRole;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
@@ -42,7 +43,9 @@ public class Member implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.roles.getRole()));
+        return authorities;
     }
 
     @Override

3e398178e0c8eda1f9728bedb4d50ba3d877ae7f
한성현 <hsh5349@naver.com>
2023-05-08 17:37:47 +0900

    #23 add: 서비스 로직 예외 서비스 로직에서 발생하는 예외를 던질 객체 생성

---
+++ backend/src/main/java/com/whereismyhome/exception/BusinessLogicException.java
@@ -0,0 +1,14 @@
+package com.whereismyhome.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException{
+
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}

327ebd4d6d8b05f84ec7fbd9876c02db716eff77
HanSungHyeon <hsh5349@naver.com>
2023-05-09 21:47:42 +0900

    #23 update: 예외 가공 작업 넘김 advice에서 가공하던 예외를 ErrorResponse로 넘김

--- backend/src/main/java/com/whereismyhome/exception/ExceptionController.java
+++ backend/src/main/java/com/whereismyhome/exception/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.whereismyhome.exception;
 
+import com.whereismyhome.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,8 +12,7 @@ public class ExceptionController {
 
     @ExceptionHandler
     public ResponseEntity businessLogicException(BusinessLogicException e) {
-        ExceptionCode exceptionCode = e.getExceptionCode();
 
-        return ResponseEntity.status(exceptionCode.getStatus()).body(e.getMessage());
+        return ErrorResponse.setErrorResponse(e.getExceptionCode());
     }
 }

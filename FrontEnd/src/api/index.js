a3d616a4ce972a3578c980fd17379f570e92e42f
문준호 <tjsduq0423@naver.com>
2023-05-11 17:23:38 +0900

    Delete branch #31

---
+++ FrontEnd/src/api/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const create = (baseURL, options) => axios.create({ baseURL, ...options });
+// import.meta.env.VITE_APP_API_URL
+// withCredentials: true,
+export const auth = create();
+
+// import axios from 'axios';
+
+// function create(baseURL, options) {
+// 	const instance = axios.create(Object.assign({ baseURL }, options));
+// 	return instance;
+// }
+
+// export const posts = create(`${import.meta.env.VITE_APP_API_URL}posts/`);
+// // development : http://localhost:5000/posts/
+// // production : http://localhost:5001/posts/

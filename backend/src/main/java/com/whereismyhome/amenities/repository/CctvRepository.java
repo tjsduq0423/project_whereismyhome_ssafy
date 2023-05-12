850f2cd69a3bbe9efb049b8bbbb6ce79de9b486c
한성현 <hsh5349@naver.com>
2023-05-12 17:37:58 +0900

    #46 add: 반경 내 cctv 정보 조회

--- backend/src/main/java/com/whereismyhome/amenities/repository/CctvRepository.java
+++ backend/src/main/java/com/whereismyhome/amenities/repository/CctvRepository.java
@@ -1,7 +1,15 @@
 package com.whereismyhome.amenities.repository;
 
+import com.whereismyhome.amenities.dto.response.CctvResponseDto;
 import com.whereismyhome.amenities.entity.Cctv;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CctvRepository extends JpaRepository<Cctv,Integer> {
+
+    @Query("select new com.whereismyhome.amenities.dto.response.CctvResponseDto(c.id, c.usage, c.lat, c.lng) from Cctv c where st_distance_sphere(Point(c.lng,c.lat),Point(:lng,:lat)) <= 1000")
+    List<CctvResponseDto> findByCctv(@Param("lng") String lng, @Param("lat") String lat);
 }
--- backend/src/main/java/com/whereismyhome/amenities/service/AmenService.java
+++ backend/src/main/java/com/whereismyhome/amenities/service/AmenService.java
@@ -1,7 +1,9 @@
 package com.whereismyhome.amenities.service;
 
 import com.whereismyhome.amenities.dto.response.BusResponseDto;
+import com.whereismyhome.amenities.dto.response.CctvResponseDto;
 import com.whereismyhome.amenities.repository.BusRepository;
+import com.whereismyhome.amenities.repository.CctvRepository;
 import com.whereismyhome.houseinfo.dto.HouseResponseDto;
 import com.whereismyhome.houseinfo.repository.HouseInfoRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import java.util.List;
 public class AmenService {
     private final HouseInfoRepository houseInfoRepository;
     private final BusRepository busRepository;
+    private final CctvRepository cctvRepository;
 
     //반경 내 아파트 정보 조회
     public List<HouseResponseDto> findApt(String lng, String lat) {
@@ -27,5 +30,12 @@ public class AmenService {
         List<BusResponseDto> busList = busRepository.findByBus(lng, lat);
 
         return busList;
+    }
+
+    //변경 내 cctv 정보조회
+    public List<CctvResponseDto> findCctv(String lng, String lat) {
+        List<CctvResponseDto> cctvList = cctvRepository.findByCctv(lng, lat);
+
+        return cctvList;
     }
 }

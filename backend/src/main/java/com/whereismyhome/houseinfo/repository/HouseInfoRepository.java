d84438d5fa5cf9b755f11f644447873d41d75dc3
HanSungHyeon <hsh5349@naver.com>
2023-05-13 01:09:09 +0900

    #46 update:아파트 정보 쿼리 성능 향상 진행

--- backend/src/main/java/com/whereismyhome/houseinfo/repository/HouseInfoRepository.java
+++ backend/src/main/java/com/whereismyhome/houseinfo/repository/HouseInfoRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface HouseInfoRepository extends JpaRepository<HouseInfo, Long> {
-
-    @Query("select new com.whereismyhome.houseinfo.dto.HouseResponseDto(h.aptCode,h.apartmentName,h.lng,h.lat) from HouseInfo h where st_distance_sphere(h.localPoint,Point(:lng,:lat)) <= 1000")
+    @Query("select new com.whereismyhome.houseinfo.dto.HouseResponseDto(h.aptCode,h.apartmentName,h.lng,h.lat) from HouseInfo h where st_contains(st_buffer(Point(:lng,:lat),0.01),h.localPoint)")
     List<HouseResponseDto> findByApt(@Param("lng") String lng, @Param("lat") String lat);
 }

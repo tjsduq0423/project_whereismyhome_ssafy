a3085c43fa3e7f7d57b49bf4efd5926ec932dee5
문준호 <tjsduq0423@naver.com>
2023-05-15 17:54:41 +0900

    Update feature #56
    - 라우터 가드+ requiredAuth 등록
    
    fixes #56

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -33,41 +33,49 @@ const router = createRouter({
     {
       path: '/notice',
       name: 'Notice',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/NoticeView.vue'),
     },
     {
       path: '/notice/create',
       name: 'NoticeCreate',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/NoticeCreateView.vue'),
     },
     {
       path: '/notice/:id',
       name: 'NoticeDetail',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/NoticeDetailView.vue'),
     },
     {
       path: '/notice/edit/:id',
       name: 'NoticeEdit',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/NoticeEditView.vue'),
     },
     {
       path: '/qna',
       name: 'QnA',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/QnAView.vue'),
     },
     {
       path: '/qna/create',
       name: 'QnACreate',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/QnACreateView.vue'),
     },
     {
       path: '/qna/:id',
       name: 'QnADetail',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/QnADetailView.vue'),
     },
     {
       path: '/qna/edit/:id',
       name: 'QnAEdit',
+      meta: { requiredAuth: true },
       component: () => import('@/views/board/QnAEditView.vue'),
     },
     {
@@ -76,5 +84,20 @@ const router = createRouter({
     },
   ],
 });
+import { useAlert } from '@/composables/alert';
+import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
+const authStore = useAuthStore();
+const { userInfo } = storeToRefs(authStore);
 
+const { vAlert, vSuccess } = useAlert();
+
+router.beforeEach(to => {
+  if (to.meta.requiredAuth && userInfo === null) {
+    vAlert('로그인 해주세요!!!');
+    vSuccess();
+    return { name: 'Login' };
+  }
+  // 알람. 권한 ,리다이렉트
+});
 export default router;

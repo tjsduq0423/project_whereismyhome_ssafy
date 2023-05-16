c49dd79cdfdfa30bb13485935232c443b3467e36
문준호 <tjsduq0423@naver.com>
2023-05-16 17:56:24 +0900

    Update feature #58
    - my page view ui 구성 ing...
    
    fixes #58

--- FrontEnd/src/views/main/MyPageView.vue
+++ FrontEnd/src/views/main/MyPageView.vue
@@ -1,8 +1,29 @@
 <template>
   <AppContent class="img">
-    <div class="row mt-5 text-center">
-      <div class="col">
-        <h1>MyPage</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col text-center">
+          <h1 class="mb-5">
+            {{ userInfo.name }}
+          </h1>
+          <h5>userInfo.email</h5>
+        </div>
+      </div>
+
+      <div class="row tabs">
+        <div class="col">
+          <RouterLink :to="{ name: MyQnA }">My QnA</RouterLink>
+        </div>
+        <div class="col">
+          <RouterLink :to="{ name: MyBookMark }">My BookMark</RouterLink>
+        </div>
+      </div>
+
+      <div class="row">
+        <hr />
+      </div>
+      <div class="row body">
+        <div class="col">content</div>
       </div>
     </div>
   </AppContent>
@@ -10,11 +31,21 @@
 
 <script setup>
 import AppContent from '@/components/AppContent.vue';
+import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
+
+const authStore = useAuthStore();
+const { userInfo } = storeToRefs(authStore);
 </script>
 
 <style scoped>
 .img {
   background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)),
-    url('');
+    url('@/assets/img/apartment04jpg.jpg');
+}
+
+a:hover,
+a.router-link-active {
+  border-bottom: 2px solid rgba(92, 180, 218, 0.998);
 }
 </style>

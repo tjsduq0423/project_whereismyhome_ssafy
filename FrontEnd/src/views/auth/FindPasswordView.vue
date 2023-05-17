d837a3015f27e1ea5c3c785baff79178a6b749cd
문준호 <tjsduq0423@naver.com>
2023-05-17 11:01:10 +0900

    Update feature #62
    - 이메일 인증, 비밀번호 찾기 ui 모달 생성
    
    closes #58

--- FrontEnd/src/views/auth/FindPasswordView.vue
+++ FrontEnd/src/views/auth/FindPasswordView.vue
@@ -23,25 +23,81 @@
             placeholder="Ex - example@naver.com"
           />
         </div>
-        <button
-          type="button"
-          class="d-grid col-12 btn btn-success text-truncate"
-        >
-          비밀번호 찾기
-        </button>
+        <div class="row m-0">
+          <button
+            class="btn btn-success text-truncate"
+            type="button"
+            @click="sendEmail"
+          >
+            비밀번호 찾기
+          </button>
+          <AppModal
+            :is-open="modalOpen"
+            @close-modal="
+              () => {
+                modalOpen = false;
+              }
+            "
+          >
+            <div class="row">
+              <label for="code" class="form-label"> Authentication Code</label>
+              <input
+                type="text"
+                class="form-control"
+                id="id"
+                v-model="code"
+                placeholder="Enter your Authentication code"
+              />
+              <small id="codeHelp" class="form-text text-muted"
+                >이메일로 전송된 코드를 입력해주세요</small
+              >
+            </div>
+            <div class="row">
+              <button
+                type="button"
+                @click="showPassword"
+                class="btn btn-success text-truncate my-3"
+              >
+                인증
+              </button>
+            </div>
+          </AppModal>
+        </div>
       </form>
     </div>
   </AppContent>
 </template>
 
 <script setup>
+import AppModal from '@/components/AppModal.vue';
 import AppContent from '@/components/AppContent.vue';
 import AppCardHeader from '@/components/AppCardHeader.vue';
 import { ref } from 'vue';
-// import { useAlert } from '@/composables/alert';
-// const { vAlert, vSuccess } = useAlert();
+import { useAlert } from '@/composables/alert';
+const { vAlert } = useAlert();
 const id = ref('');
 const email = ref('');
+
+const code = ref('');
+const modalOpen = ref(false);
+
+const sendEmail = () => {
+  if (validate()) {
+    //메일 보내기
+    modalOpen.value = true;
+  }
+};
+const validate = () => {
+  if (!id.value || !email.value) {
+    vAlert('입력 형식을 맞춰주세요');
+    return false;
+  }
+  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value) === false) {
+    vAlert('이메일 형식이 맞지 않습니다.');
+    return false;
+  }
+  return true;
+};
 </script>
 
 <style scoped>

2e97b1e399b229dd7bbe761aa760f4875a2e6a1b
한성현 <hsh5349@naver.com>
2023-05-18 13:11:40 +0900

    #64 add: 카카오 로그인 버튼 생성

--- FrontEnd/src/views/auth/LoginView.vue
+++ FrontEnd/src/views/auth/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
   <AppContent class="img">
     <AppCardHeader>로그인</AppCardHeader>
+    <div>
+      <img src="@/assets/img/kakao_login_medium.png" @click="kakao" />
+    </div>
     <div class="card-body d-flex flex-column justify-content-center">
       <form @submit.prevent>
         <div class="mb-4">
@@ -35,18 +38,10 @@
           로그인
         </button>
         <div class="d-flex justify-content-between mt-3">
-          <button
-            type="button"
-            class="btn btn-outline-danger text-truncate"
-            @click="goFindPW"
-          >
+          <button type="button" class="btn btn-outline-danger text-truncate" @click="goFindPW">
             비밀번호 찾기
           </button>
-          <button
-            type="button"
-            class="btn btn-outline-success text-truncate"
-            @click="goSignup"
-          >
+          <button type="button" class="btn btn-outline-success text-truncate" @click="goSignup">
             회원 가입
           </button>
         </div>
@@ -56,36 +51,41 @@
 </template>
 
 <script setup>
-import AppCardHeader from '@/components/AppCardHeader.vue';
-import AppContent from '@/components/AppContent.vue';
-import { useRouter } from 'vue-router';
-import { useAuthStore } from '@/stores/auth.js';
-import { ref } from 'vue';
-import { useAlert } from '@/composables/alert';
+import AppCardHeader from '@/components/AppCardHeader.vue'
+import AppContent from '@/components/AppContent.vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth.js'
+import { ref } from 'vue'
+import { useAlert } from '@/composables/alert'
 
-const { vAlert, vSuccess } = useAlert();
-const { validateMember } = useAuthStore();
-const id = ref('');
-const password = ref('');
+const { vAlert, vSuccess } = useAlert()
+const { validateMember } = useAuthStore()
+const id = ref('')
+const password = ref('')
 
-const router = useRouter();
+const router = useRouter()
 
 const login = () => {
   try {
-    validateMember(id.value, password.value);
-    vSuccess('로그인 성공');
-    router.push({ name: 'Home' });
+    validateMember(id.value, password.value)
+    vSuccess('로그인 성공')
+    router.push({ name: 'Home' })
   } catch (err) {
-    vAlert('ID와 비밀번호를 다시 확인해주세요');
-    console.error(err);
+    vAlert('ID와 비밀번호를 다시 확인해주세요')
+    console.error(err)
   }
-};
+}
 const goFindPW = () => {
-  router.push({ name: 'FindPassword' });
-};
+  router.push({ name: 'FindPassword' })
+}
 const goSignup = () => {
-  router.push({ name: 'SignUp' });
-};
+  router.push({ name: 'SignUp' })
+}
+const kakao = () => {
+  window.open(
+    'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=6c779f7199f5aa77f349e6165df482bc&redirect_uri=http://localhost:5500/login'
+  )
+}
 </script>
 
 <style scoped>

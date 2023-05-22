561d9e63e7de85dfe6e22e920d9bd904f6093d89
joonho <tjsduq0423@uos.ac.kr>
2023-05-22 11:26:04 +0900

    Update #76 fixes error import
    closes #76

--- FrontEnd/src/views/main/my/MyBookMarkView.vue
+++ FrontEnd/src/views/main/my/MyBookMarkView.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import InfinityScroll from '@/components/AppInfinityScroll.vue';
+import InfinityScroll from '@/components/features/AppInfinityScroll.vue';
 </script>
 
 <style lang="scss" scoped></style>
